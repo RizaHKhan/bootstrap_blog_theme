bdd456f52f1204dd128ea149e6d3635209affbb7
RizaHKhan <khanriza@gmail.com>
2020-04-27 19:53:14 -0400

    added thumbnails to posts

--- functions.php
+++ functions.php
@@ -1,5 +1,12 @@
 <?php
 
+function theme_support()
+{
+  add_theme_support('post-thumbnails');
+}
+
+add_action('after_setup_theme', 'theme_support');
+
 function register_styles()
 {
   $version = wp_get_theme()->get('Version');
